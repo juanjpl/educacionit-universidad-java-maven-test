128b9b3123328be0394cb42f7728b48e606889a3
juanjpl <juan_jpl@hotmail.com>
2025-08-08 12:30:11 -0300

    upload files

---
+++ modulo-a/src/main/java/org/modulo/a/MainApp.java
@@ -0,0 +1,15 @@
+package org.modulo.a;
+
+import org.modulo.b.App;
+
+public class MainApp 
+{
+    public static void main( String[] args )
+    {
+       
+    	//Crear una instancia del modulo-b
+    	var app = new App();
+    	
+    	System.out.println("Fin de modulo-a");
+    }
+}
---
+++ modulo-b/src/main/java/org/modulo/b/App.java
@@ -0,0 +1,10 @@
+package org.modulo.b;
+
+
+public class App 
+{
+    public App()
+    {
+        System.out.println( "Se crea el app en el modulo B");
+    }
+}
